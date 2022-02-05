665e2ef5250b67904f21cc91272605c3646c7781
Emre Kesler <emrek@DESKTOP>
2022-02-05 03:53:15 +0300

    Responsive edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,53 +2,27 @@ module.exports = {
   jit: true,
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1400px',
+    },
     extend: {
+
       fontFamily: {
         'rubik': "'Rubik', serif",
       },
-      colors: {
-        cpurple: {
-          100: "#fdcede",
-          200: "#fb9cbd",
-          300: "#fa6b9c",
-          400: "#f8397b",
-          500: "#f6085a",
-          600: "#c50648",
-          700: "#940536",
-          800: "#620324",
-          900: "#310212"
-        },
-        cgreen: {
-          100: "#f5ffcc",
-          200: "#ebff99",
-          300: "#e2ff66",
-          400: "#d8ff33",
-          500: "#ceff00",
-          600: "#a5cc00",
-          700: "#7c9900",
-          800: "#526600",
-          900: "#293300"
-        },
-        cgray: {
-          100: "#d3d3d3",
-          200: "#a6a6a6",
-          300: "#7a7a7a",
-          400: "#4d4d4d",
-          500: "#212121",
-          600: "#1a1a1a",
-          700: "#141414",
-          800: "#0d0d0d",
-          900: "#070707"
-        },
-      },
       fontSize: {
-        'tiny': '.875rem',
+        'tiny': '.800rem',
+        'calc': 'calc(1.265625rem + 0.1875vw)',
       },
       backgroundImage: {
         'blog1': "url('https://blogzine.webestica.com/assets/images/blog/1by1/01.jpg')",
         'coin': "url('https://images.unsplash.com/photo-1643488056768-fe017dc164b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80')",
         'food': "url('https://images.unsplash.com/photo-1632158930341-46604b637a0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80')",
-        'hottub': "url('https://images.unsplash.com/photo-1532926381893-7542290edf1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')"
+        'car': "url('https://img.redbull.com/images/w_2929/q_auto,f_auto/redbullcom/2021/10/10/pu9vpczmlgzwilqrlufm/red-bull-racing')"
       },
       spacing: {
         '128': '35rem',
@@ -57,5 +31,34 @@ module.exports = {
     },
 
   },
-  plugins: [],
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          width: '100%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+
+          '@screen sm': {
+            maxWidth: '96%',
+          },
+          '@screen md': {
+            maxWidth: '94%',
+          },
+          '@screen lg': {
+            maxWidth: '94%',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1200px',
+          },
+        }
+      })
+    }
+  ],
 }
